18f61d9cbff88790f7df835b62d16abda94b29ae
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-02-08 13:46:28 +0530

    Added JS Operators

---
+++ JAVASCRIPT/03_operations.js
@@ -0,0 +1,35 @@
+//Assignment
+var name = "Samkit"
+
+//Arithmetic
+console.log(4+3)
+console.log(10-3)
+console.log(7*1)
+console.log(28/4)
+console.log(7**3)
+console.log(22%15)
+
+var i = 1;
+console.log(i++) //1 - print and increment
+console.log(i) //2 - print
+console.log(++i) //3 - increment and print
+
+//Relational
+console.log(1==1)
+console.log(1=='1') //true
+console.log(1==='1') //false - strict equality check
+console.log(1!='1') //false
+console.log(1!=='1') //true
+
+//Comparative
+console.log(3>=4)
+
+//Logical
+console.log(3<4 && 5<6)
+console.log(3<4 || 3>4)
+console.log(true ^ false) //1
+console.log(true ^ true) //0
+
+//Bitwise Operators
+console.log(10 & 6) //2
+console.log(10 | 6) //14
