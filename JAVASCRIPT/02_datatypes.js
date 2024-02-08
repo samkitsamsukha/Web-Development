0b1390aebd17874175d53fc0df201da774e33e61
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-02-08 13:33:52 +0530

    Added JS Data Types

---
+++ JAVASCRIPT/02_datatypes.js
@@ -0,0 +1,42 @@
+// PRIMITIVE
+
+//String
+var name = "Samkit"
+var title = 'Samsukha'
+console.log(title)
+console.log(typeof title)
+
+//Number
+var age = 19
+console.log(age)
+console.log(typeof(age))
+
+//BigInt
+var bignum = 123n;
+console.log(typeof bignum)
+
+//NaN
+console.log(typeof NaN) //Number
+
+//null
+console.log(typeof null) //Object
+
+var a
+console.log(typeof a) //undefined
+
+//boolean
+console.log(typeof true)
+
+//symbol
+var s1 = Symbol()
+
+/**********************************************************************************************************/
+
+// OBJECTS
+var person = {
+    user : "Samkit",
+    dob : 141005,
+    sport : "cricket"
+}
+console.log(person)
+console.log(typeof person)
