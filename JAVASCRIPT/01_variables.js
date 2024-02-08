ec8613254806ee367227dda4df00d8a5f205bcad
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-02-08 13:19:15 +0530

    Changes in CSS and Addition of JS Variables

---
+++ JAVASCRIPT/01_variables.js
@@ -0,0 +1,20 @@
+var name //Declaration
+
+console.log(name)
+
+var age = 19 //Declaration + Initialisation
+
+console.log(age)
+
+/*
+    Name a variable
+    1. a-z/A-Z/_/$/0-9
+    2. Keywords not to be used
+    3. No spaces in between
+    4. Should be meaningful
+    5. Case Sensitive
+*/
+
+var hobby
+var nationality
+// var %country
