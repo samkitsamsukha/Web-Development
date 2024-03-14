feaa783805f3704f4611f3fdca98cc84b07caa75
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-03-14 23:08:48 +0530

    Added Node.Js  files

---
+++ Node.js/4_names.js
@@ -0,0 +1,7 @@
+//local
+const secret = "SUPER SECRET"
+//share
+const manita = "Manita";
+const vineet = "Vineet";
+
+module.exports = {manita, vineet}; // to make something available from this module
